b688e7bb71bfba6d61b33f3e0f88fa899ae3f215
Dylan Fitzgerald <dylanfitzgerald@learnzillion.com>
2017-10-01 22:11:13 -0600

    Move most of Pooly.Server's brains to Pooly.PoolServer

--- lib/pooly/server.ex
+++ lib/pooly/server.ex
@@ -2,142 +2,42 @@ defmodule Pooly.Server do
   use GenServer
   import Supervisor.Spec
 
-  defmodule State do
-    defstruct [:sup, :worker_sup, :size, :workers, :monitors, :mfa]
-    @type t :: %State{sup: pid, worker_sup: pid, size: non_neg_integer,
-                      workers: list, monitors: list, mfa: {any, any, any}}
-  end
-
   # API
 
-  def start_link(sup, pool_config) do
-    # FIXME Start off with named process (via opts = [name: @name])
-    GenServer.start_link(__MODULE__, [sup, pool_config], name: __MODULE__)
+  def start_link(pools_config) do
+    GenServer.start_link(__MODULE__, pools_config, name: __MODULE__)
   end
 
-  def checkout, do: GenServer.call(__MODULE__, :checkout)
-
-  def checkin(worker_pid), do: GenServer.cast(__MODULE__, {:checkin, worker_pid})
-
-  def status, do: GenServer.call(__MODULE__, :status)
+  # FIXME interpolation into atom here smells a little off.
+  # However, note that this is representative of someone else's mistakes,
+  # and is a reminder that GenServer.call/3's first argument _must be an atom_.
+  def checkout(pool_name), do: GenServer.call(:"#{pool_name}Server", :checkout)
+  def checkin(pool_name, worker_pid), do: GenServer.cast(:"#{pool_name}Server", {:checkin, worker_pid})
+  def status(pool_name), do: GenServer.call(:"#{pool_name}Server", :status)
 
   # Callbacks
 
   ## invoked upon GenServer.start_link/3
-  ## stores the invoking (top-level) supervisor's pid in state, then inits pool
-  def init([sup, pool_config]) when is_pid(sup) do
-    Process.flag(:trap_exit, true)
-    monitors = :ets.new(:monitors, [:private])
-    init(pool_config, %State{sup: sup, monitors: monitors})
+  ## iterates through the configuration, sending `:start_pool` to itself
+  def init(pools_config) do
+    pools_config |> Enum.each(fn(pool_config) ->
+      send(self(), {:start_pool, pool_config})
+    end)
+
+    {:ok, pools_config}
   end
 
-  ## Valid `pool_config`: `[mfa: {SampleWorker, :start_link, []}, size: 5]`
-  ## Note it's an _ordered_ list, so we can h|t our way through it and know
-  ## which keys to expect.
-  ##
-  ## Use this pattern to build up `state`.
-
-  ## Pattern match for `mfa` option:
-  def init([{:mfa, mfa}|tail], state),   do: init(tail, %{state | mfa: mfa})
-
-  ## Pattern match for `size` options:
-  def init([{:size, size}|tail], state), do: init(tail, %{state | size: size})
-
-  ## (In theory, match for any other desired options here)
-
-  ## Drop any other options:
-  def init([_|tail], state),             do: init(tail, state)
-
-  ## Options list empty, so assume state is built up
-  def init([], state) do
-    send(self(), :start_worker_supervisor)
-    {:ok, state}
-  end
-
-  def handle_call(:checkout, {from_pid, _ref}, %{workers: workers, monitors: monitors} = state) do
-    case workers do
-      [ worker|tail ] ->
-        # exist un-checked-out workers in pool
-        ref = Process.monitor(from_pid)
-        true = :ets.insert(monitors, {worker, ref})
-        {:reply, worker, %{state | workers: tail}}
-
-      [] ->
-        # all workers checked out
-        {:reply, :noproc, state}
-    end
-  end
-
-  def handle_call({:status, _from, %{workers: workers, monitors: monitors} = state}) do
-    {:reply, {length(workers), :ets.info(monitors, :size)}, state}
-  end
-
-  def handle_cast({:checkin, worker}, %{workers: workers, monitors: monitors} = state) do
-    case :ets.lookup(monitors, worker) do
-      [{pid, ref}] ->
-        # TODO this looks like a prime candidate to refactor using `with`
-        true = Process.demonitor(ref)
-        true = :ets.delete(monitors, pid)
-        {:noreply, %{state | workers: [pid|workers]}}
-      [] ->
-        {:noreply, state}
-    end
-  end
-
-  def handle_info(:start_worker_supervisor, state = %{sup: sup, mfa: mfa, size: size}) do
-    {:ok, worker_sup} = Supervisor.start_child(sup, supervisor_spec(mfa))
-    workers = prepopulate(size, worker_sup)
-    {:noreply, %{state | worker_sup: worker_sup, workers: workers}}
-  end
-
-  def handle_info({:DOWN, ref, _, _, _}, state = %{monitors: monitors, workers: workers}) do
-    case :ets.match(monitors, {:"$1", ref}) do
-      [[pid]] ->
-        true = :ets.delete(monitors, pid)
-        new_state = %{state | workers: [pid|workers]}
-        {:noreply, new_state}
-
-      [[]] ->
-        {:noreply, state}
-    end
-  end
-
-  def handle_info({:EXIT, pid, _reason},
-    state = %{monitors: monitors, workers: workers, worker_sup: worker_sup}) do
-    case :etc.lookup(monitors, pid) do
-      [{pid, ref}] ->
-        true = Process.demonitor(ref)
-        true = :ets.delete(monitors, pid)
-        new_state = %{state | workers: [new_worker(worker_sup)|workers]}
-        {:noreply, new_state}
-
-      [{}] ->
-        {:noreply, state}
-    end
+  def handle_info({:start_pool, pool_config}, state) do
+    {:ok, _pool_sup} = Supervisor.start_child(Pooly.PoolsSupervisor, supervisor_spec(pool_config))
+    {:noreply, state}
   end
 
   # Private Functions
 
-  defp supervisor_spec(mfa) do
-    opts = [restart: :temporary]
-    supervisor(Pooly.WorkerSupervisor, [mfa], opts)
+  defp supervisor_spec(pool_config) do
+    # Supervisor spec must be unique, so vary the ID field:
+    opts = [id: :"%{pool_config[:name]}Supervisor"]
+    supervisor(Pooly.PoolSupervisor, [pool_config], opts)
   end
 
-  defp prepopulate(size, sup), do: prepopulate(size, sup, [])
-  defp prepopulate(size, _sup, workers) when size < 1, do: workers
-  defp prepopulate(size, sup, workers) do
-    prepopulate(size-1, sup, [new_worker(sup) | workers])
-  end
-
-  defp new_worker(sup) do
-    # n.b. `sup` here receives worker_sup, so we're adding children
-    # to `Pooly.WorkerSupervisor`. This has already had its child_spec
-    # defined with start_child(supervisor_pid, child_spec) in
-    # handle_info(:start_worker_supervisor) and uses a `:simple_one_for_one`
-    # restart strategy, so the child spec is predefined. Instead, we use
-    # the alternate start_child/2(supervisor, [args]) syntax.
-    # Cf. h(Supervisor.start_child/2).
-    {:ok, worker} = Supervisor.start_child(sup, [[]])
-    worker
-  end
 end
